cbf9b1a0967880ae1315170bc1b51fd0c78fe154
Emiliano <emit3reyes@gmail.com>
2021-07-23 16:46:22 -0500

    Push

--- artecultura/settings.py
+++ artecultura/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e+=ionzs=@qgx6wdwg=2+sx7ow2w72_)-ud)-ty!hk#1!csysy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["herokuapp.com"]
 
 
 # Application definition
