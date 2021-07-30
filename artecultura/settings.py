259f61e3fa9a68258c76897b91db2d3380bb19bf
Emiliano <emit3reyes@gmail.com>
2021-07-30 13:41:33 -0500

    VIdeo

--- artecultura/settings.py
+++ artecultura/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middles.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'artecultura.urls'
